b66b0a188f61a33a04a9f88c7a4dd024e46acd7b
Jumoke Alonge <ariyikemaryann@gmail.com>
2022-11-23 21:57:59 +0100

    created the mark up for the frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react'
+import axios from 'axios'
 function App() {
+
+// const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=8229df065635b9b2acc2968d5ee298a6`
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+       <div className="container">
+        <div className="top">
+           <div className="location">
+            <p>Dallas</p>
+           </div>
+           <div className="temp">
+             <h1>60&#176;F</h1>
+           </div>
+           <div className="description">
+            <p>Clouds</p>
+           </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>65&#176;F</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            <p>12 MPH</p>
+          </div>
+        </div>
+       </div>
+     
     </div>
   );
 }
